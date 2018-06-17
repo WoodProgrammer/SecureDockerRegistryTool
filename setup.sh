3033cb63fc31ad5444c916df5742bb1d358759e9
Emir Özbir <emirozbir@hotmail.com>
2018-06-17 13:41:43 +0300

    Nginx Copying added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "DOMAIN NAME"
+read domain
+
 echo "SSL Signing and creating"
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+
 echo "Nginx Key : /etc/nginx/ssl/nginx.key"
 echo "Nginx Crt : /etc/nginx/ssl/nginx.crt"
 
@@ -17,6 +21,12 @@ read password
 
 docker run --entrypoint htpasswd registry:2 -Bbn $username $password > /etc/nginx/ssl/auth/htpasswd
 
+echo "Domain Name Added and config file copying to the /etc/nginx/conf.d"
+
+sed -i -e 's/DOMANIN/$domain/g' registry.conf
+cp registry.conf /etc/nginx/conf.d/.
+
+ 
 
 echo "Registry Service Created ! "
 
@@ -26,3 +36,6 @@ docker run -d -p 5000:5000 --restart=always --name registry \
   -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/registry.password \
   registry:2
+
+
+
