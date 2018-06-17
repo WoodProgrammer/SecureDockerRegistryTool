4c32d5ff999d300624924a019b9616e6fe81822c
Emir Özbir <emirozbir@hotmail.com>
2018-06-17 13:37:51 +0300

    Setup.sh Added Nginx Configured

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# SecureREGISTRY 
+# SecureREGISTRY 
+
+Tool for Creating Secure Docker Registry behind Proxy
+
+## Native Requirements 
+* NGINX
+* DOCKER 
+* SHELL (this tool design for Linux Server)
+
+
+## Usage 
+	 
+     $ git clone https://github.com/WoodProgrammer/SecureDockerRegistryTool.git
+     
+     $ cd SecureDockerRegistryTool
+     
+     $ chmod +x setup.sh
+     
+     $ ./setup.sh 
+     
+If you do instruction shown as above, you can input to script what is want .
+
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "SSL Signing and creating"
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+echo "Nginx Key : /etc/nginx/ssl/nginx.key"
+echo "Nginx Crt : /etc/nginx/ssl/nginx.crt"
+
+
+echo "Initializing to Swarm"
+
+docker swarm init 
+
+echo "Registry Username"
+read username
+
+echo "Registry Password"
+read password
+
+docker run --entrypoint htpasswd registry:2 -Bbn $username $password > /etc/nginx/ssl/auth/htpasswd
+
+
+echo "Registry Service Created ! "
+
+docker run -d -p 5000:5000 --restart=always --name registry \
+  -v /etc/nginx/conf.d/:/auth \
+  -e "REGISTRY_AUTH=htpasswd" \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/registry.password \
+  registry:2
